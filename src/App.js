50d03de75d940aba5a6c348930168c3dbe104a28
mikeybcode <thejachal@hotmail.co.uk>
2022-02-17 10:43:51 +0000

    updated displaying of tweets

--- src/App.js
+++ src/App.js
@@ -1,42 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
 import fetch from 'cross-fetch';
-import React,{useEffect} from 'react'
+import React,{useState} from 'react'
+import {Card,Button} from "react-bootstrap";
+import {TwitterTweetEmbed} from "react-twitter-embed";
+import format from 'date-fns/format';
+
 function App() {
 
-  
+  const [tweets,setTweets] = useState(null);
 
-  const tweetid = "1490746602964852744";
+  //const compid = "1490746602964852744";
+
+  const retweets200 = "903709298902970368";
+
+  //const ellentweet2mil = "440322224407314432";
 
   const fetchTweets = async(tweetid) => {
 
   const url = `http://127.0.0.1:5500/`;
 
-
-    const config = {method : "POST", headers:{ 'Content-Type': 'application/json',"Access-Control-Allow-Origin" : true}, body: JSON.stringify({ "tweetid" : `${tweetid}`}) };
+  const config = {method : "POST", headers:{ 'Content-Type': 'application/json',"Access-Control-Allow-Origin" : true}, body: JSON.stringify({ "tweetid" : `${tweetid}`}) };
 
     try{
       const tweets = await fetch(url,config);
-      const tweetJson = await tweets.json();
-      return await tweets;
+      const tweetsJson = await tweets.json();
+
+      console.log(tweetsJson);
+      console.log(tweetsJson.length);
+      setTweets(tweetsJson);
+      // return tweetsJson;
     }catch(err){
       console.log(err.data);
-      return { Error : "no tweet found"}
+      // return { Error : "no tweet found"}
     }
   }
-   
 
-   useEffect(() => {
-    fetchTweets(tweetid);
-  },[]);
+  const displayTweets = () => {
+
+
+    if(tweets !== null && tweets !== undefined){
+      
+      return tweets.map(tweet =>
+      <Card key={tweet.id}style={{ width: '18rem'}} bg="success">
+        <Card.Body>
+        <Card.Title>{tweet.username}</Card.Title>
+        <Card.Text>
+            {tweet.id}
+            {tweet.created_at}
+        </Card.Text>
+        </Card.Body>
+      </Card>);
+    }else{
+      return <h1>no tweets</h1>
+    }
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>    tweets:
-        {/* {result} */}
-        </p>
+      <header className="container">
+        <div className="AppBody">
+          <div style={{width:"50%"}}>
+          <TwitterTweetEmbed style={{width: "auto"}} tweetId={retweets200}/>
+          </div>
+          <div>
+          <Button variant="success" type="submit" onClick={() => fetchTweets(retweets200)}>LOAD RETWEETS</Button>
+          {displayTweets()}
+          </div>
+        </div>
       </header>
     </div>
   );
