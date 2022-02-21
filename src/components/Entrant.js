fee8e28c25656488e9ca61425e1064f00e146519
mikeybcode <thejachal@hotmail.co.uk>
2022-02-21 05:18:33 +0000

    changed entrant display

--- src/components/Entrant.js
+++ src/components/Entrant.js
@@ -2,20 +2,21 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import format from "date-fns/format";
 
-export const Entrant = ({ entry, isValid = true }) => {
+export const Entrant = ({ entry, retweetCount }) => {
   return (
     <Row
       key={entry.id}
       style={{ fontWeight: "bold" }}
       className={
-        isValid
+        parseInt(retweetCount) < 3
           ? "bg-success border border-dark align-items-center "
           : "bg-danger border border-dark align-items-center "
       }
     >
+      {console.log(retweetCount)}
       {/* <Col>{key}</Col> */}
       <Col>{entry.username}</Col>
-      <Col>{!isValid ? "POSSIBLE BOT" : ""}</Col>
+      <Col>{retweetCount}</Col>
       (RETWEETED ON<br></br>
       {format(new Date(entry.created_at), "dd/MMMM")})
       <Col>
